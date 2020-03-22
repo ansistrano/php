8f86803db8fa88674a4342edc0caf9e1c545b8b8
Carlos Buenosvinos <carlos.buenosvinos@gmail.com>
2020-03-22 11:04:34 +0100

    Try event based strategy

--- src/Composer/InstallerPlugin.php
+++ src/Composer/InstallerPlugin.php
@@ -3,11 +3,15 @@
 namespace Ansistrano\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capable;
+use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
 
-class InstallerPlugin implements PluginInterface, Capable
+class InstallerPlugin implements PluginInterface, EventSubscriberInterface, Capable
 {
     public function activate(Composer $composer, IOInterface $io)
     {
@@ -24,4 +28,29 @@ class InstallerPlugin implements PluginInterface, Capable
             'Composer\Plugin\Capability\CommandProvider' => 'Ansistrano\Composer\CommandProvider'
         );
     }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            PackageEvents::POST_PACKAGE_INSTALL => array(
+                array('onPostPackageInstall', 0)
+            )
+        );
+    }
+
+    public function onPostPackageInstall(PackageEvent $packageEvent)
+    {
+        print_r($packageEvent);
+    }
+
+//    public function onPreFileDownload(PreFileDownloadEvent $event)
+//    {
+//        $protocol = parse_url($event->getProcessedUrl(), PHP_URL_SCHEME);
+//
+//        if ($protocol === 's3') {
+//            $awsClient = new AwsClient($this->io, $this->composer->getConfig());
+//            $s3RemoteFilesystem = new S3RemoteFilesystem($this->io, $event->getRemoteFilesystem()->getOptions(), $awsClient);
+//            $event->setRemoteFilesystem($s3RemoteFilesystem);
+//        }
+//    }
 }
